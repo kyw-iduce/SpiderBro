667fb8d6af5bfc55018c5afb7c97df4bd2b8c5dc
Andrea Birotti <andrea.birotti@gmail.com>
2017-10-29 18:30:49 +0000

    Modified run_server.py to add the x-robots-tags for the saturday and sunday scenarios (with mappings hardcoded)

--- web/html/run_server.py
+++ web/html/run_server.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
+
+import BaseHTTPServer
 import SimpleHTTPServer
-import SocketServer
 
-PORT = 8000
+"""
+Usage:
+    python run_server.py [port] 
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+Example:
+    python run_server.py 8000 
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+N.B.: Mapping is hardcoded
 
-print "serving at port", PORT
-httpd.serve_forever()
+"""
+
+
+class CustomHTTPRequestHandler(
+    SimpleHTTPServer.SimpleHTTPRequestHandler
+):
+
+    def end_headers(self):
+        self.send_new_headers()
+
+        SimpleHTTPServer\
+            .SimpleHTTPRequestHandler\
+            .end_headers(self)
+
+    def send_new_headers(self):
+        headers_by_path = {
+            '/saturday/1.html': 'x-robots-tag: index, nofollow',
+            '/sunday/1.html': 'x-robots-tag: noindex, follow'
+        }
+
+        request_path = self.path
+        if request_path in headers_by_path:
+            key, value = headers_by_path[request_path]\
+                .split(":", 1)
+            self.send_header(key, value)
+
+
+if __name__ == '__main__':
+    BaseHTTPServer.test(
+        HandlerClass=CustomHTTPRequestHandler,
+        ServerClass=BaseHTTPServer.HTTPServer,
+        protocol="HTTP/1.1")
